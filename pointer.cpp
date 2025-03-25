28be1dd9a440a09f922ccda522695bc81d25caeb
John Money <94144017+MoneyJohn@users.noreply.github.com>
2025-03-25 10:13:32 -0700

    pointer

---
+++ pointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+int main(){
+    int a = 5;
+    std::cout<<a<<std::endl;
+    std::cout<<&a<<std::endl;//& gives the address of the int a 
+
+    int* pointer = &a; //keepig the address of a as a variable pointer
+
+
+    int arr[6]={1,2,3,4,5,6};
+    
+
+
+}
